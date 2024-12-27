b26f30502905f0f02c9ee79342dd2b36328ed3f8
Wilson <wilson.dacles@gmail.com>
2024-12-27 12:27:05 +0800

    Navigation changes

--- scripts.js
+++ scripts.js
@@ -22,3 +22,23 @@ document.addEventListener("DOMContentLoaded", function () {
 const footer = document.querySelector("footer p");
 const currentYear = new Date().getFullYear();
 footer.innerHTML = `&copy; ${currentYear} Wilson Dacles. All rights reserved.`;
+
+
+// Function to show the selected section and hide the others
+ function showSection(sectionId) {
+    // Hide all sections
+    var sections = document.querySelectorAll('.section');
+    sections.forEach(function(section) {
+        section.classList.remove('active');
+    });
+
+// Show the selected section
+    var activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
+
+// Optionally, show the About section by default on page load
+    window.onload = function() {
+        showSection('about');
+    };
+ 
